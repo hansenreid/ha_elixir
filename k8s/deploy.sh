2baf78aa2c2ad59ec64fc34d8a34e709afdae204
Reid Hansen <rhansen@influxdata.com>
2022-02-22 11:31:08 -0600

    Remove pwd call

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -6,7 +6,6 @@ helm install ha-elixir-postgres --set auth.postgresPassword=postgres bitnami/pos
 kubectl run ha-elixir-postgres-postgresql-client --rm --tty -i --restart='Never' --namespace default --image docker.io/bitnami/postgresql:14.1.0-debian-10-r80 --env="PGPASSWORD=postgres" \
         --command -- psql --host ha-elixir-postgres-postgresql -U postgres -d postgres -p 5432 -c 'CREATE DATABASE ha_elixir_prod'
 
-pwd
 ./k8s/migrate.sh
 
 kubectl apply -f k8s/deployments.yaml
